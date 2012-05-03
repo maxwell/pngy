2f6b2144f7ed88c9f6075e06afc3b13ff136828c
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-05-02 21:03:01 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pngy::Application.config.secret_token = '13174a245e41fe058d42bd48a32737c8d903e6aa4bdf6d706ca93f6ace154e8f219ebf8234b149a443ad676c5f9c4d4dd119963df321a5752f999de11ef24162'
